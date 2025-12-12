584ec7b49ab05b1b58019b576b0094ac625cd1d0
RylandLoyd <rylandjloyd@gmail.com>
2025-12-11 20:10:59 -0800

    Add LabEquipment class definition

---
+++ LabEquipment.h
@@ -0,0 +1,27 @@
+#ifndef LABEQUIPMENT_H
+#define LABEQUIPMENT_H
+
+#include "Resource.h"
+
+class LabEquipment : public Resource {
+private:
+    string equipmentType;
+    bool fragile;
+
+public:
+    LabEquipment(int id, const string& name, const string& equipmentType, bool fragile);
+
+    // Getters
+    const string& getEquipmentType() const;
+    bool isFragile() const;
+
+    // Setters
+    void setEquipmentType(const string& type);
+    void setFragile(bool f);
+
+    // Overrides
+    string getCategory() const override;
+    void display(ostream& os = cout) const override;
+};
+
+#endif
