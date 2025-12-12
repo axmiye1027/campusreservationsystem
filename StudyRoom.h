cc77a60e40cae8226d71fd030a33bd312ebcacb0
RylandLoyd <rylandjloyd@gmail.com>
2025-12-11 20:09:22 -0800

    Add StudyRoom class definition with getters and setters

---
+++ StudyRoom.h
@@ -0,0 +1,30 @@
+#ifndef STUDYROOM_H
+#define STUDYROOM_H
+
+#include "Resource.h"
+
+class StudyRoom : public Resource {
+private:
+    int capacity;
+    string location;
+    string availabilityHours;
+
+public:
+    StudyRoom(int id, const string& name, int capacity, const string& location, const string& availabilityHours);
+
+    // Getters
+    int getCapacity() const;
+    const string& getLocation() const;
+    const string& getAvailabilityHours() const;
+
+    // Setters
+    void setCapacity(int cap);
+    void setLocation(const string& loc);
+    void setAvailabilityHours(const string& hours);
+
+    // Overrides
+    string getCategory() const override;
+    void display(ostream& os = cout) const override;
+};
+
+#endif
