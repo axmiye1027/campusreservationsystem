d0dc58d3c59ec30e3b88cb578f362b087bc35da5
RylandLoyd <rylandjloyd@gmail.com>
2025-12-11 20:13:26 -0800

    Refactor Resource class and remove derived classes

--- Resource.h
+++ Resource.h
@@ -12,45 +12,19 @@ protected:
     string name;
 
 public:
-    Resource(int id, string name) : id(id), name(move(name)) {}
+    Resource(int id, string name);
     virtual ~Resource() = default;
 
-    int getId() const { return id; }
-    const string& getName() const { return name; }
+    int getId() const;
+    const string& getName() const;
+
+     void setName(const string& newName);
 
     virtual void display(ostream& out) const = 0;
+    virtual string getCategory() const = 0;
+
+    ostream& operator<<(ostream& os, const Resource& r);
 };
 
-class StudyRoom : public Resource {
-    int capacity;
-    string location;
-    string hours;
-
-public:
-    // Study room with capacity, location, and operating hours attributes.
-    StudyRoom(int id, string name, int capacity, string location, string hours)
-        : Resource(id, move(name)), capacity(capacity), location(move(location)), hours(move(hours)) {}
-
-    void display(ostream& out) const override {
-        out << "Study Room [ID " << id << "] " << name
-            << " | Capacity: " << capacity
-            << " | Location: " << location
-            << " | Hours: " << hours;
-    }
-};
-
-class LabEquipment : public Resource {
-    string type;
-
-public:
-    // Lab equipment tagged by a descriptive type.
-    LabEquipment(int id, string name, string type)
-        : Resource(id, move(name)), type(move(type)) {}
-
-    void display(ostream& out) const override {
-        out << "Lab Equipment [ID " << id << "] " << name
-            << " | Type: " << type;
-    }
-};
 
 #endif
