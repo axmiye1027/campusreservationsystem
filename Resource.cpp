78f393dd3865eb2869b099866a43c18c4c88acd7
RylandLoyd <rylandjloyd@gmail.com>
2025-12-11 20:08:13 -0800

    Implement Resource class with constructor and methods

---
+++ Resource.cpp
@@ -0,0 +1,22 @@
+#include "Resource.h"
+
+// Constructor
+Resource::Resource(int id, const std::string& name)
+    : id(id), name(name) {
+}
+
+// Getters
+int Resource::getId() const
+{ return id; }
+const string& Resource::getName() const
+{ return name; }
+
+void Resource::setName(const string& newName) {
+    name = newName;
+}
+
+// Operator overload
+ostream& operator<<(ostream& os, const Resource& r) {
+    r.display(os);
+    return os;
+}
