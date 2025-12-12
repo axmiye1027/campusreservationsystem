17ad739bcd66a902862bdd4e3553ca5565777a3b
axmiye1027 <axmiye1027@gmail.com>
2025-12-12 11:15:18 -0800

    Create User.h

---
+++ User.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class User
+{
+private:
+  string username;
+public:
+  User();
+  bool isUser(istream& file, string userName);
+  bool isAdmin(istream&file, string userName);
+
+  string getUsername();
+  void setUsername(string userName);
+};
