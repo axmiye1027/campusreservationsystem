fa51420a332e2015df1aaa839aaf174d5aaa8676
RylandLoyd <rylandjloyd@gmail.com>
2025-12-11 20:11:17 -0800

    Implement LabEquipment class with getters and setters

---
+++ LabEquipment.cpp
@@ -0,0 +1,27 @@
+#include "LabEquipment.h"
+
+// Constructor
+LabEquipment::LabEquipment(int id,const string& name,const string& equipmentType, bool fragile): Resource(id, name),
+    equipmentType(equipmentType), fragile(fragile) {
+}
+
+// Getters
+const string& LabEquipment::getEquipmentType() const { return equipmentType; }
+bool LabEquipment::isFragile() const { return fragile; }
+
+// Setters
+void LabEquipment::setEquipmentType(const string& type) { equipmentType = type; }
+void LabEquipment::setFragile(bool f) { fragile = f; }
+
+// Overrides
+std::string LabEquipment::getCategory() const {
+    return "LabEquipment";
+}
+
+void LabEquipment::display(ostream& os) const {
+    os << "[Lab Equipment]\n"
+        << "  ID: " << id << "\n"
+        << "  Name: " << name << "\n"
+        << "  Type: " << equipmentType << "\n"
+        << "  Fragile: " << (fragile ? "Yes" : "No") << "\n";
+}
