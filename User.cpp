25e0e8d591fe33f7e0f03f27939f5e272f31c830
RylandLoyd <rylandjloyd@gmail.com>
2025-12-14 13:09:46 -0800

    Create User.cpp

---
+++ User.cpp
@@ -0,0 +1,25 @@
+#include "User.h"
+
+// Default constructor
+User::User() : username("") {}
+
+// Parameterized constructor
+User::User(string userName) : username(userName) {}\
+
+
+
+
+
+
+
+// Getter
+string User::getUsername()
+{
+    return username;
+}
+
+// Setter
+void User::setUsername(string userName)
+{
+    username = userName;
+}
