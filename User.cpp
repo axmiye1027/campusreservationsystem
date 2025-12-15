02c2322be2af4c208855172c6318cd0558522a22
ElizabethShahbazian <148819798+ElizabethShahbazian@users.noreply.github.com>
2025-12-15 12:56:38 -0800

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -6,11 +6,21 @@ User::User() : username("") {}
 // Parameterized constructor
 User::User(string userName) : username(userName) {}
 
+// Implementation of Saving to File 
+bool User::saveToFile (const string& path) const {
+ofstream out (path);
+if (!out) return false;
+out << username << '\n';
+return true;
+}
 
-
-
-
-
+//Implementaion of Loading from File
+bool User::loadToFile (const string& path) const {
+ifstream in (path);
+if (!in) return false;
+getlin (in, username);
+return true;
+}
 
 // Getter
 string User::getUsername()
