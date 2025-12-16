9e25ea79033f21e10a62e19305118e55d29e2b1d
axmiye1027 <axmiye1027@gmail.com>
2025-12-16 00:26:45 -0800

    Update main.cpp
    Commented out the original main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
-#include "TestSuite.h"
+// #include "TestSuite.h"
 
-int main() {
-    TestSuite::runAllTests();
-    return 0;
+// int main() {
+//     TestSuite::runAllTests();
+//     return 0;
+// }
+
+#include "User.h"
+#include "Menu.h"
+
+int main()
+{
+	const int BORDER_SIZE = 50;
+	User user;
+	int option;
+
+
+	option = -1;
+
+	while (option != 0)
+	{
+		displayMainMenu();
+		getAndValidateMenuInput(option);
+
+		switch(option)
+		{
+		case 0: break;
+		case 1: createAccount(BORDER_SIZE, user); break;
+		case 2:
+			break;
+
+		case 3: //Calls reservation functions
+			break;
+
+		getAndValidateMenuInput(option);
+
+		}
+
+
+	return 0;
+	}
 }
 
