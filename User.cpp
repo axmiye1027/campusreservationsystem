bce8d7901011cf0f1f1b26790e95278820b04a09
axmiye1027 <axmiye1027@gmail.com>
2025-12-18 18:33:21 -0800

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,35 +1,75 @@
 #include "User.h"
+#include "UserManager.h"
 
-// Default constructor
-User::User() : username("") {}
+User::User()
+{
+	role = "Student";
+	isLoggedIn = false;
 
-// Parameterized constructor
-User::User(string userName) : username(userName) {}
+	//PROC - To create a default username, we need to know what the last username was.
+	ifstream fin;
+	string lastUsername; //the last default username created, gives access to the last number used
+	string lastNumber;//the number used in lastUsername, to be incremented to create a new default username
+	fin.open("used_default_usernames.txt");
 
-// Implementation of Saving to File 
-bool User::saveToFile (const string& path) const {
-ofstream out (path);
-if (!out) return false;
-out << username << '\n';
-return true;
+	if (!fin || !(fin >> lastUsername)) //if file does not exist/cannot open OR the file is empty
+	{
+		lastNumber = "0";
+	}
+	else
+	{
+		//if there are usernames, we want to keep reading until we hit the end;
+		while (fin >> lastUsername) //reads a username only if the read succeeds
+		{
+		} //automatically traverses through the file
+		lastNumber = lastUsername.substr(4);
+	}
+
+	fin.close();
+	username = "User" + lastNumber;
+
 }
 
-//Implementaion of Loading from File
-bool User::loadFromFile (const string& path) {
-ifstream in (path);
-if (!in) return false;
-getline (in, username);
-return true;
+User::User(string userName, string userRole)
+{
+	role = userRole;
+	isLoggedIn = false;
+	username = userName;
 }
 
-// Getter
+
 string User::getUsername()
 {
-    return username;
+  return username;
 }
 
-// Setter
+string User::getRole()
+{
+	return role;
+}
+bool User::getIsLoggedIn()
+{
+	return isLoggedIn;
+}
+
+
+
 void User::setUsername(string userName)
 {
-    username = userName;
+	username = userName;
 }
+
+void User::setRole(string role)
+{
+	this->role = role;
+}
+
+void User::setIsLoggedIn(bool isLoggedIn)
+{
+	this->isLoggedIn = isLoggedIn;
+}
+
+
+
+
+
